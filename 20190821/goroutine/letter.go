74bb9cab4de9c14107f647108760963b8757a88d
hhe0 <deng2296917779@163.com>
2019-08-22 11:08:23 +0800

    并发

--- 20190821/goroutine/letter.go
+++ 20190821/goroutine/letter.go
@@ -5,20 +5,48 @@ import (
 	"time"
 )
 
-func main() {
-	go printNumbers()
-	printLetter()
-}
-
-func printNumbers() {
+func printNumber1() {
 	for i := 0; i < 10; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Printf("%d", i)
+		fmt.Printf("%d ", i)
 	}
 }
 
-func printLetter() {
-	for i := 'A'; i < 'A'+10; i++ {
-		fmt.Printf("%c", i)
+func printNumber2() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("%d ", i)
 	}
+}
+
+func printLetters1() {
+	for i := 'A'; i < 'A' + 10; i++ {
+		fmt.Printf("%c ", i)
+	}
+}
+
+func printLetters2() {
+	for i := 'A'; i < 'A' + 10; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("%c ", i)
+	}
+}
+
+func print1() {
+	printNumber1()
+	printLetters1()
+}
+
+func goPrint1() {
+	go printNumber1()
+	go printLetters1()
+}
+
+func goPrint2() {
+	go printNumber2()
+	go printLetters2()
+}
+
+func main() {
+	goPrint2()
+	time.Sleep(1 * time.Minute)
 }
